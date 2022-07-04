2a8fc20490adbd00c024586c7644a26fcc663417
Zahaak Khan <khazahaa@sheridancollege.ca>
2022-07-04 14:44:52 -0400

    - Created Applivcation Project Folders - Prepared Application Services and Request Pipeline - Updated ReadMe

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// method sets up the shared objects required by applications using the MVC Framework and the Razor view engine
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// use static content (js,css, images, ...)
+app.UseStaticFiles();
+
+//registers the MVC Framework as a source of endpoints using default convention for mapping requests to classes and methods
+app.MapDefaultControllerRoute();
 
 app.Run();
