628084efe163a5dda1d2c01c5435ee600f3ede08
Zahaak Khan <khazahaa@sheridancollege.ca>
2022-07-04 18:58:47 -0400

    Added Pagination

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,8 @@ namespace SportsStore_ECommerce_Web_App.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        // specify products per page
+        public int PageSize = 5;
 
         // using dependency injection, and its approach allows the HomeController object to access the application’s repository
         // through the IStoreRepository interface without knowing which implementationclass has been configured.
@@ -14,9 +16,11 @@ namespace SportsStore_ECommerce_Web_App.Controllers
             repository=repo;
         }
 
-        public IActionResult Index()
-        {
-            return View(repository.Products);
-        }
+        public IActionResult Index(int productPage = 1) =>
+         View(repository.Products //get the Product objects
+             .OrderBy(p=>p.ProductID) //order them by primary key
+             .Skip((productPage-1)*PageSize) //skip over the products that occur before the start of the current page
+             .Take(PageSize)); //take the number of products specified by PageSize
+        
     }
 }
